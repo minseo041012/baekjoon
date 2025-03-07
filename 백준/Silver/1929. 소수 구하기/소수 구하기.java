751f8dd71fa61ac70d84969913f935c20d844e1f
minseo041012 <dkfltn1012@naver.com>
2025-03-07 15:39:59 +0900

    [Silver III] Title: 소수 구하기, Time: 220 ms, Memory: 21116 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -29,17 +29,19 @@ public class Main{
         br.close();
     }
 
+    //에라토스테네스의 체
     public static boolean[] sieve(int n, int m){
         boolean[] prime = new boolean[n + 1];
         Arrays.fill(prime, true);
 
         prime[0] = prime[1] = false;
 
+        //i가 i*i를 초과하면 그 이상의 값에서 나누어질 가능성 없음
         for (int i = 2; i <= Math.sqrt(n); i++){
             //소수라면 뒤에 오는 코드 스킵
             if(prime[i] == false)
                 continue;
-            //2부터 배수에 해당하는 수 true로 변경
+            //2부터 배수에 해당하는 수 false로 변경
             for(int j = i * i; j <= n; j += i)
                 prime[j] = false;
         }
