a8585b4504100a2c3139d408bd2f24b8e4da7d2d
minseo041012 <dkfltn1012@naver.com>
2025-05-23 21:38:05 +0900

    [Silver II] Title: 랜선 자르기, Time: 212 ms, Memory: 17216 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int k = Integer.parseInt(st.nextToken());
+        long n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[k];
+        long result = 0;
+
+        //가지고 있는 랜선 길이 배열에 저장
+        for(int i = 0; i < k; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        //n이 1일 경우 배열 0번째 인덱스 출력 
+        if(n == 1|| (arr[0] == arr[arr.length - 1] && k > 1 & k == n))
+            bw.write(String.valueOf(arr[0]));
+        else {
+            result = binary_search(n, arr);
+            bw.write(String.valueOf(result));
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static long binary_search(long n, int arr[]){
+        //overflow 방지
+        long lo = 0;
+        long hi = arr[arr.length - 1];
+        long mid = 0;
+
+        while(lo + 1 < hi){
+            mid = (lo + hi) / 2;
+            int sum = 0;
+            //mid로 구해지는 랜선의 개수
+            for (int i = 0; i < arr.length; i++){
+                sum += arr[i] / mid;
+            }
+            if (sum < n)
+                hi = mid;
+            else if(sum >= n)
+                lo = mid;
+        }
+        return lo;
+    }
+}
