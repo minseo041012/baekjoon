49eabbc6e86bcc2ddf0041ba591665ed0736a425
minseo041012 <dkfltn1012@naver.com>
2025-05-24 20:35:05 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 360 ms, Memory: 42552 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -17,32 +17,41 @@ public class Main{
             //괄호가 하나도 없는 경우는 yes로 출력
             if (arr[0].equals("."))
                 break;
+            //문장 끝까지 돌기
             for(int i = 0; i < arr.length; i++){
-                //"("일 경우 small 증가
+                //"("일 경우 small 증가, st에 add
                 if(arr[i].equals("(")){
                     st.add(arr[i]);
                     small++;
                 }
-                //")"일 경우 small 감소 & 사전에 "("이 존재했어야함
-                if(arr[i].equals(")") && small > 0){
+                //")"일 경우
+                if(arr[i].equals(")")){
+                    //이전에 "("가 없었다면 no입력 후 탈출
+                    if (small == 0){
+                        sb.append("no" + "\n");
+                        break;
+                    }
                     String temp = st.pop();
+                    //"("가 아닌 다른게 존재한다면 no 입력 후 탈출
                     if(!temp.equals("(")) {
                         sb.append("no" + "\n");
                         break;
                     }
                     small--;
                 }
-                else if (arr[i].equals(")") && small == 0){
-                    sb.append("no" + "\n");
-                    break;
-                }
-                //"["일 경우, big 증가
+                //"["일 경우 big 증가, st에 add
                 if(arr[i].equals("[")) {
                     st.add(arr[i]);
                     big++;
                 }
-                //"]"일 경우 big 감소 & 사전에 "]"이 존재했어야함
-                if(arr[i].equals("]") && big > 0){
+                //"]"일 경우
+                if(arr[i].equals("]")){
+                    //이전에 "]"가 존재하지 않았다면 no 입력 후 탈출
+                    if (big == 0){
+                        sb.append("no" + "\n");
+                        break;
+                    }
+                    //"["가 아닌 다른게 존재한다면 no 입력 후 탈출
                     String temp = st.pop();
                     if(!temp.equals("[")) {
                         sb.append("no" + "\n");
@@ -50,11 +59,6 @@ public class Main{
                     }
                     big--;
                 }
-                else if (arr[i].equals("]") && big == 0){
-                    sb.append("no" + "\n");
-                    break;
-                }
-
                 //문장의 끝 확인하기
                 if(arr[i].equals(".")){
                     if(small == 0 && big == 0)
