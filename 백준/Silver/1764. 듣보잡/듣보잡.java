b2bdf34d2420f647a98975a109662c5420a79f0b
minseo041012 <dkfltn1012@naver.com>
2025-07-03 19:12:46 +0900

    [Silver IV] Title: 듣보잡, Time: 284 ms, Memory: 29188 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -6,7 +6,6 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
@@ -29,8 +28,10 @@ public class Main{
                 result.add(temp);
             }
         }
+        //듣도 보도 못한 놈들 이름순으로 정렬
         Collections.sort(result);
         bw.write(String.valueOf(count) + "\n");
+        //list 순회
         for(int i = 0; i < result.size(); i++)
             bw.write(result.get(i) + "\n");
 
