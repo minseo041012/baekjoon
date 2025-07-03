1efbe6b5bd59d8c82c7edb0b5e0e48d7ffb27412
minseo041012 <dkfltn1012@naver.com>
2025-07-03 19:11:04 +0900

    [Silver IV] Title: 듣보잡, Time: 300 ms, Memory: 29700 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int count = 0;
+        int size = 0;
+        HashSet<String> set= new HashSet<>();
+        List<String> result = new ArrayList<>();
+
+        //듣도 못한 사람 hashset에 추가
+        for(int i = 0; i < n; i++)
+            set.add(br.readLine());
+        //보도 못한 사람
+        for(int i = 0; i < m; i++) {
+            String temp = br.readLine();
+            size = set.size();
+            set.add(temp);
+            //중복인 경우 -> 듣도 보도 못한 사람
+            if(size == set.size()){
+                count++;
+                result.add(temp);
+            }
+        }
+        Collections.sort(result);
+        bw.write(String.valueOf(count) + "\n");
+        for(int i = 0; i < result.size(); i++)
+            bw.write(result.get(i) + "\n");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
