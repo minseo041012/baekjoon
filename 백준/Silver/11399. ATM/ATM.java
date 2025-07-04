80756df4513c6799271326512fd20e64fa230134
minseo041012 <dkfltn1012@naver.com>
2025-07-04 14:05:16 +0900

    [Silver IV] Title: ATM, Time: 112 ms, Memory: 14492 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -11,11 +11,14 @@ public class Main{
         int[] m = new int[n];
         int sum = 0;
         int temp = 0;
+        //문자열을 " " 기준으로 나누기
         arr = br.readLine().split(" ");
+        //string -> int
         for(int i = 0; i < n; i++)
             m[i] = Integer.parseInt(arr[i]);
+        //오름차순으로 정렬
         Arrays.sort(m);
-
+        //누적합 이용해서 sum에 더해주기
         for(int i = 0; i < n; i++){
             temp += m[i];
             sum += temp;
