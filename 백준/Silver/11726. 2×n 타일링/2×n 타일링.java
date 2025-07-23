a458ca2b7317f183e852de8c4cfeba59dd7ddb78
minseo041012 <dkfltn1012@naver.com>
2025-07-23 18:32:02 +0900

    Update 2×n 타일링.java

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -12,7 +12,9 @@ public class Main{
         num_method[1] = 1;
         num_method[2] = 2;
         num_method[3] = 3;
-        //n이
+        //n이 3일 경우로 가정해서 설명
+        //우측 타일은 세로 직사각형 or 가로 직사각형으로 끝나는 방식
+        //n - 1에서 세로 직사각형을 붙이고, n - 2에서 가로 직사각형을 붙이면 총 n의 개수가 나타남
         for(int i = 4; i <= n; i++)
             num_method[i] = (num_method[i - 1] + num_method[i - 2]) % 10007;
 
