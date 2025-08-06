ec84e0a7fa660a5bbd9f0c9a5218de4b4c1b7944
minseo041012 <dkfltn1012@naver.com>
2025-08-07 01:49:31 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -7,21 +7,20 @@ public class Main{
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         long n = Long.parseLong(br.readLine());
-        long temp = n;
         long result = 0;
         long num = (long) (Math.log10(n) + 1);//몇의 자리 숫자인지 확인하는 변수
 
 
         for(long i = 0; i < num; i++){
             //각 범위에 따라 자리 수 곱해주는 식
-            if(temp >= Math.pow(10, i + 1))
+            //10~99면 90 * 2, 100~999면 900 * 3
+            if(n >= Math.pow(10, i + 1))
                 result += (i + 1) * 9 * (long)Math.pow(10, i);
             else {
-                n -= (long)Math.pow(10, i);
-                result += (n + 1) * (i + 1);
+                n -= (long)Math.pow(10, i);//나머지 값을 구한 후
+                result += (n + 1) * (i + 1);//n+1인 이유는 xx0, x0 처럼 일의 자리가 0인 경우 고려
                 break;
             }
-            //n -= 9 * (long)Math.pow(10, i);
         }
 
         bw.write(String.valueOf(result));
