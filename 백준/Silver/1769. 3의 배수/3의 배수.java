1aee7b608cce378112de9e1a8027ace95dcf8e65
minseo041012 <dkfltn1012@naver.com>
2025-08-10 00:15:26 +0900

    [Silver V] Title: 3의 배수, Time: 272 ms, Memory: 48624 KB -BaekjoonHub

--- 백준/Silver/1769. 3의 배수/3의 배수.java
+++ 백준/Silver/1769. 3의 배수/3의 배수.java
@@ -7,35 +7,27 @@ public class Main{
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
-        String[] arr = br.readLine().split("");
-        int num = 0;
-        int count;
+        String s = br.readLine();
+        int count = 0;
 
-        //입력된 값의 총 합 구하기
-        for(int i = 0; i < arr.length; i++){
-            num += Integer.parseInt(arr[i]);
-        }
-        if(arr.length != 1)
-            count = 1;//변환 과정 횟수
-        else
-            count = 0;
+        
 
         while(true){
+            int sum = 0;
             //num이 일의 자리 수일 경우
-            if(0 < num && num < 10){
+            if(s.length() == 1){
                 break;
             }
-            int temp = num;
-            num = 0;
-            while(temp!=0){
-                num += temp % 10;
-                temp /= 10;
+            //입력된 값의 총 합 구하기
+            for(int i = 0; i < s.length(); i++){
+                sum += Integer.parseInt(String.valueOf(s.charAt(i)));
             }
+            s = String.valueOf(sum);
             count++;
         }
 
         //3의 배수이면 횟수와 yes 출력
-        if(num % 3 == 0)
+        if(Integer.parseInt(String.valueOf(s)) % 3 == 0)
             sb.append(count).append("\n").append("YES");
         //3의 배수가 아니면 no 출력
         else
