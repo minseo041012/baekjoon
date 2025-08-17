846667c7720bd65c1784796e21da38f1d6b97387
minseo041012 <dkfltn1012@naver.com>
2025-08-17 18:59:12 +0900

    [Silver III] Title: 게임, Time: 104 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.java
+++ 백준/Silver/1072. 게임/게임.java
@@ -13,7 +13,7 @@ public class Main{
 
         double z = Math.floor((double)y * 100 / x);//처음 승률
 
-        //100퍼센트 제외하고 이진탐색으로 count 얻기
+        //99퍼이면, 이미 패배 전적이 있기 때문에 100으로 올라갈 수 없다.
         if(z <= 98)
             count = binary(0, x, x, y, z);
 
@@ -29,6 +29,8 @@ public class Main{
     static long binary(long left, long right, long x, long y, double z){
         while(left <= right){
             long mid = (left + right) / 2;
+            //y/x*100 대신 아래처럼 사용한 이유
+            //처음에 y * 100을 곱함으로써, 소수점 계산에서 발생되는 오차를 피할 수 있다.
             long temp_per = ((mid + y) * 100) / (x + mid);
 
             //승률이 1보다 크면 right 변경
