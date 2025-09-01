e61bfcf6590b535c0643ab5c67843d3e83d08205
minseo041012 <dkfltn1012@naver.com>
2025-09-02 00:16:54 +0900

    [Bronze I] Title: 단어 공부, Time: 248 ms, Memory: 20160 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -6,47 +6,30 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] arr = br.readLine().split("");
-        HashMap<String, Integer> count = new HashMap<>();
+        int[] alpha = new int[26];
+        String s = br.readLine();
 
-        for(int i = 0; i < arr.length; i++){
-            String upper = arr[i].toUpperCase();//출력을 대문자로 하기에 처음부터 대문자로 받기
-            //알파벳이 이미 있으면 count 값만 변경
-            if(count.containsKey(upper)){
-                int temp = count.get(upper);
-                count.replace(upper, ++temp);
-            }
+        for(int i = 0; i < s.length(); i++){
+            //대문자일 경우
+            if('A' <= s.charAt(i) && s.charAt(i) <= 'Z')
+                alpha[s.charAt(i) - 'A']++;//-'A'는 인덱스 벗어나지 않도록 하기 위해
             else
-                count.put(upper, 1);
-        }
-        //hashmap에서 value를 기준으로 정렬하는 방법은 list로 변경해서 collection sort함수 이용해서 정렬
-        List<String> list = new ArrayList<>(count.keySet());
-        Collections.sort(list, (value1, value2) -> count.get(value2).compareTo(count.get(value1)));
-        int max = 0;
-        int i = 0;
-        String temp = "";
-
-        for(String key : list){
-            //1개의 알파벳만 있으면 출력 후 탈출
-            if(list.size() == 1) {
-                bw.write(String.valueOf(key));
-                break;
-            }
-            //빈도 수가 같은 개 여러 개인 경우
-            if(i == 1 && max == count.get(key)) {
-                bw.write(String.valueOf("?"));
-                break;
-            }
-            //가장 많이 사용된 알파벳 출력
-            else if(i == 1 && max > count.get(key)) {
-                bw.write(String.valueOf(temp));
-                break;
-            }
-            i++;
-            max = count.get(key);
-            temp = key;//이전 알파벳을 저장
+                alpha[s.charAt(i) - 'a']++;
         }
 
+        int max = -1;
+        char answer = '?';
+
+        for(int i = 0; i < 26; i++){
+            if(alpha[i] > max) {
+                max = alpha[i];
+                answer = (char)(i + 65);//대문자로 출력해야 하기 때문에, 65 더함
+            }
+            else if(max == alpha[i])
+                answer = '?';
+        }
+
+        bw.write(String.valueOf(answer));
         bw.flush();
         bw.close();
         br.close();
