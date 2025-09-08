e00897d0432f4016a1fcb1f558f1d79257e134da
minseo041012 <dkfltn1012@naver.com>
2025-09-09 02:37:48 +0900

    [Silver II] Title: 최소 힙, Time: 268 ms, Memory: 26304 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -9,15 +9,19 @@ public class Main{
 
         int n = Integer.parseInt(br.readLine());
 
+        //priorQueue 내부적으로 최소 힙(min-heap) 구현되어있음
         PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>(new Comparator<Integer>(){
             public int compare(Integer o1, Integer o2){
                 return - Integer.compare(o2, o1);
             }
         });
+        //PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Comparator.reverseOrder());
+        //위의 코드도 가능
 
         for(int i = 0; i < n; i++){
             int input = Integer.parseInt(br.readLine());
 
+            //0이 들어오면 가장 작은 값을 출력 -> 가장 작은 값=우선순위 높음
             if(input == 0) {
                 if(minHeap.isEmpty())
                     sb.append(0).append("\n");
