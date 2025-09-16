e6844322a326e446b076bbd1f04500440e9d6d9c
minseo041012 <dkfltn1012@naver.com>
2025-09-16 21:47:45 +0900

    [Silver V] Title: 폴리오미노, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.java
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -7,24 +7,27 @@ public class Main{
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
 
+        //구분자를 "."로, 3번째 인자를 true로 주면서 토큰으로도 지정
         StringTokenizer st = new StringTokenizer(br.readLine(), "//.", true);
         int count = 0;
 
         while(st.hasMoreTokens()){
             String token = st.nextToken();
-            //.일 경우, 이전에 X의 개수에 맞춰 aaaa, bb 출력
+            //.일 경우
             if(token.equals(".")){
                 sb.append(".");
             }
             else{
                 int length = token.length();
                 String temp = "";
+                //사전순으로 출력해야함으로, AAAA 먼저 검사
                 if(length > 3) {
                     temp += "AAAA".repeat(length / 4);
                     length %= 4;
                 }
                 if(length % 2 == 0)
                     temp += ("BB".repeat(length / 2));
+                //홀수인 경우는 A or B로 덮을 수 없기에 -1 출력
                 else{
                     sb.setLength(0);
                     sb.append("-1");
