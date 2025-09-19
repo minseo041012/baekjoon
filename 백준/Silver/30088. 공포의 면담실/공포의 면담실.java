204132c90be4a4de7e75b66c86c3165ddceb47c1
minseo041012 <dkfltn1012@naver.com>
2025-09-19 20:35:46 +0900

    [Silver V] Title: 공포의 면담실, Time: 112 ms, Memory: 14516 KB, Score: 36 point -BaekjoonHub

---
+++ 백준/Silver/30088. 공포의 면담실/공포의 면담실.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int department = Integer.parseInt(br.readLine());
+        int[] sum = new int[department];
+        int result = 0;
+
+        for(int i = 0; i < department; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int employee = Integer.parseInt(st.nextToken());
+            for(int j = 0; j < employee; j++)
+                sum[i] += Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(sum);
+
+        int temp = 0;
+        for(int i = 0; i < department; i++){
+            temp += sum[i];
+            result += temp;
+        }
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
